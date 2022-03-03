5a80a44f17f6b591b94616677da8f45192d61e38
Tawfek .M <tawfekaltaae3@gmail.com>
2022-03-03 18:01:44 +0300

    .

--- api/config.php
+++ api/config.php
@@ -5,7 +5,7 @@ $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 
-$SupportedCountries = [ 'Bahrain', 'Cyprus', 'Egypt', 'Iran', 'Iraq', 'Israel', 'Jordan', 'Kuwait', 'Lebanon', 'Oman', 'Palestine', 'Qatar', 'Saudi-Arabia', 'Syria', 'Turkey', 'UAE', 'Yemen'];
+$SupportedCountries = ['China','Bahrain', 'Cyprus', 'Egypt', 'Iran', 'Iraq', 'Israel', 'Jordan', 'Kuwait', 'Lebanon', 'Oman', 'Palestine', 'Qatar', 'Saudi-Arabia', 'Syria', 'Turkey', 'UAE', 'Yemen'];
 
 
 define('SUPPORTED_CONUTRIES', $SupportedCountries);
